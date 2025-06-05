2da690e4dba5de13e6617f915f9c4fbfbfcec13a
Evgeniy Kungurov <johnk2280@gmail.com>
2025-06-05 22:07:09 +0500

    [add] snacks.lua

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,25 @@
+return {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+        dashboard = {
+            -- your dashboard configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            sections = {
+                { section = "header" },
+                {
+                    icon = " ",
+                    -- title = "Keymaps",
+                    section = "keys",
+                    indent = 2,
+                    padding = 2,
+                    gap = 1,
+                },
+                { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 2, gap = 1 },
+                { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+                { section = "startup" },
+            },
+        },
+    },
+}
