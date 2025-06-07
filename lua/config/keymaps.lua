fa52267f3010e1ee7bc39852a10966d557c191c2
Evgeniy Kungurov <johnk2280@gmail.com>
2025-06-07 18:41:16 +0500

    [upd] keymaps.lua - added movements in insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,15 +8,23 @@ local map = vim.keymap.set
 map("i", "jj", "<ESC>")
 
 -- Terminal
-map("t", "jj", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
-map("t", "<ESC>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+map("t", "jj", "<C-\\><C-N>", { desc = "Escape from terminal mode" })
+map("t", "<ESC>", "<C-\\><C-N>", { desc = "Escape from terminal mode" })
 
 -- Bufferline
-map("n", "<Tab>", ":BufferLineCycleNext<CR>")
-map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
+map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 
 -- Resize window using <Shift> arrow keys - override default settings: <ctrl>
 map("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Movements
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
