1de1c6d76ad2934ecfb184251bcf99d5e26332e2
Evgeniy Kungurov <johnk2280@gmail.com>
2025-06-07 17:54:20 +0500

    [upd] keymaps.lua - override default settings for resize window

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,3 +14,9 @@ map("t", "<ESC>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 -- Bufferline
 map("n", "<Tab>", ":BufferLineCycleNext<CR>")
 map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
+
+-- Resize window using <Shift> arrow keys - override default settings: <ctrl>
+map("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
