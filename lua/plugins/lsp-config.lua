210088f608f147c078e1402144829518080c523c
Evgeniy Kungurov <johnk2280@gmail.com>
2025-06-09 14:46:15 +0500

    [upd] lsp-config for eslint - deleted custom settings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,7 +12,6 @@ return {
             css_variables = require("plugins.lsp.css_variables"),
             cssls = require("plugins.lsp.cssls"),
             -- cssmodules_ls = require("plugins.lsp.cssmodules_ls"),
-            eslint = require("plugins.lsp.eslint"),
         },
     },
 }
