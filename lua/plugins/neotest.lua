4a41f000f0325006e35d4a6dbf8589bb7f9b3231
Evgeniy Kungurov <johnk2280@gmail.com>
2025-06-18 23:21:29 +0500

    [upd] neotest.lua - added dinamicaly created path to python

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,15 +3,33 @@ return {
     optional = true,
     dependencies = {
         "nvim-neotest/neotest-python",
+        "linux-cultist/venv-selector.nvim",
     },
-    opts = {
-        adapters = {
-            ["neotest-python"] = {
+    -- opts = {
+    --     adapters = {
+    --         ["neotest-python"] = {
+    --             runner = "pytest",
+    --             python = ".venv/bin/python",
+    --             args = { "--verbose", "--no-header", "-vv", "--cov" },
+    --             pytest_discover_instances = true,
+    --         },
+    --     },
+    -- },
+    opts = function(_, opts)
+        opts.adapters = opts.adapters or {}
+        table.insert(
+            opts.adapters,
+            require("neotest-python")({
                 runner = "pytest",
-                python = ".venv/bin/python",
                 args = { "--verbose", "--no-header", "-vv", "--cov" },
                 pytest_discover_instances = true,
-            },
-        },
-    },
+                python = function()
+                    local python_venv = require("venv-selector").python()
+                    -- print("python_venv = ", python_venv)
+                    return python_venv or ".venv/bin/python"
+                end,
+            })
+        )
+        return opts
+    end,
 }
