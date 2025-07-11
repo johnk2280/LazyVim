93dd90bb76f81b7e809e8b677d068d927b90c412
Evgeniy Kungurov <johnk2280@gmail.com>
2025-07-12 00:34:22 +0500

    [upd] neotest.lua - deleted dinamicaly created path to python

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,6 +12,7 @@ return {
             css_variables = require("plugins.lsp.css_variables"),
             cssls = require("plugins.lsp.cssls"),
             cssmodules_ls = require("plugins.lsp.cssmodules_ls"),
+            -- eslint = require("plugins.lsp.eslint"),
         },
     },
 }
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -5,31 +5,31 @@ return {
         "nvim-neotest/neotest-python",
         "linux-cultist/venv-selector.nvim",
     },
-    -- opts = {
-    --     adapters = {
-    --         ["neotest-python"] = {
-    --             runner = "pytest",
-    --             python = ".venv/bin/python",
-    --             args = { "--verbose", "--no-header", "-vv", "--cov" },
-    --             pytest_discover_instances = true,
-    --         },
-    --     },
-    -- },
-    opts = function(_, opts)
-        opts.adapters = opts.adapters or {}
-        table.insert(
-            opts.adapters,
-            require("neotest-python")({
+    opts = {
+        adapters = {
+            ["neotest-python"] = {
                 runner = "pytest",
+                python = ".venv/bin/python",
                 args = { "--verbose", "--no-header", "-vv", "--cov" },
                 pytest_discover_instances = true,
-                python = function()
-                    local python_venv = require("venv-selector").python()
-                    -- print("python_venv = ", python_venv)
-                    return python_venv or ".venv/bin/python"
-                end,
-            })
-        )
-        return opts
-    end,
+            },
+        },
+    },
+    -- opts = function(_, opts)
+    --     opts.adapters = opts.adapters or {}
+    --     table.insert(
+    --         opts.adapters,
+    --         require("neotest-python")({
+    --             runner = "pytest",
+    --             args = { "--verbose", "--no-header", "-vv", "--cov" },
+    --             pytest_discover_instances = true,
+    --             python = function()
+    --                 local python_venv = require("venv-selector").python()
+    --                 -- print("python_venv = ", python_venv)
+    --                 return python_venv or ".venv/bin/python"
+    --             end,
+    --         })
+    --     )
+    --     return opts
+    -- end,
 }
